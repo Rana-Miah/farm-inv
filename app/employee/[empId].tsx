36891ccf146eff40194dd82745ef2eb4ca08078e
Rana-Miah <134648732+Rana-Miah@users.noreply.github.com>
2026-07-16 07:30:26 +0000

    employee details screen created

--- app/employee/[empId].tsx
+++ app/employee/[empId].tsx
@@ -1,11 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { Button } from '@/components/ui/button';
+import { Text } from '@/components/ui/text';
+import CardWrapper from '@/components/shared/card-wrapper';
+import EmployeeUpdateForm from '@/components/form/employee-update-form';
+import Container from '@/components/shared/container';
 
 const EmployeeDetails = () => {
+    const { empId } = useLocalSearchParams<{ empId: string }>();
     return (
-        <View>
-            <Text>EmployeeDetails</Text>
-        </View>
+        <Container>
+            <CardWrapper
+                title="Update Employee Form"
+                description="Edit that field want to update"
+                headerContent={
+                    <Button
+                        size={'sm'}
+                        onPress={() => { console.log('pressed') }}
+                    >
+                        <Text>Change Password</Text>
+                    </Button>
+                }
+            >
+                <EmployeeUpdateForm employee={{
+                    employeeId: "50667",
+                    employeeTitle: "EDP",
+                    name: "RANA"
+                }} />
+            </CardWrapper>
+        </Container>
     )
 }
 
